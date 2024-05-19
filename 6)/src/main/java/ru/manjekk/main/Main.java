6220f5d7b6499d0be2a1f16e6cdbb748885b7da8
manjekk <162597536+manjekk@users.noreply.github.com>
2024-05-19 17:16:32 +0500

    6 Работа с файловой системой (./data/resource.xml)

---
+++ 6)/src/main/java/ru/manjekk/main/Main.java
@@ -0,0 +1,53 @@
+package ru.sanya1am.main;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import ru.sanya1am.resourceServer.ResourceServer;
+import ru.sanya1am.resourceServer.ResourceServerController;
+import ru.sanya1am.resourceServer.ResourceServerControllerMBean;
+import ru.sanya1am.resourceServer.ResourceServerImpl;
+import ru.sanya1am.servlets.ResourcePageServlet;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+
+public class Main {
+    static final Logger logger = LogManager.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws Exception {
+
+        int port = 8080;
+        logger.info("Starting at http://127.0.0.1:" + port);
+
+        ResourceServer resourceServer = new ResourceServerImpl();
+
+        ResourceServerControllerMBean serverStatistics = new ResourceServerController(resourceServer);
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("Admin:type=ResourceServerController");
+        mbs.registerMBean(serverStatistics, name);
+
+        Server server = new Server(port);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new ResourcePageServlet(resourceServer)), ResourcePageServlet.PAGE_URL);
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+        server.setHandler(handlers);
+
+        server.start();
+        logger.info("Server started");
+        server.join();
+    }
+}
